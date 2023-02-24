2cc4511ffe88c895103b4081898fe6d4bc9e5b56
hinosoft <ducanhvna@outlook.com>
2023-02-24 20:32:47 +0700

    Update to git ignore

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.home',  # Enable the inner home (home)
-    'apps.vantai'
+    'apps.vantai',
+    'apps.devices'
 ]
 
 MIDDLEWARE = [
